e62e2b9c8b2600b5a683aaed9b7816312a381907
Jason Farrell <imjason@gmail.com>
2014-07-21 21:36:35 -0500

    Fixed menu sizing

--- iOS/ScorePredict/ScorePredict/ViewControllers/MenuTableViewController.m
+++ iOS/ScorePredict/ScorePredict/ViewControllers/MenuTableViewController.m
@@ -11,4 +11,17 @@
 @implementation MenuTableViewController
 - (IBAction)unwindToMenuViewController:(UIStoryboardSegue *)segue { }
 
+-(void)viewDidLoad
+{
+    // get the height of the screen
+    CGFloat screenHeight = [UIScreen mainScreen].bounds.size.width;
+    
+    // set the background color of the view
+    self.view.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:1];
+    
+    // set the frame height
+    self.view.frame = CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y,
+                                 200, screenHeight);
+}
+
 @end
